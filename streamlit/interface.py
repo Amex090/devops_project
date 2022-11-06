fe76091c2a2506238995511b65b0523244b7c08f
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-11-06 21:16:30 +0300

    Update interface.py

--- streamlit/interface.py
+++ streamlit/interface.py
@@ -7,7 +7,7 @@ st.title('Определение тональности текста')
 text = st.text_input('Введите текст', 'Я люблю распределенные вычисления')
 result = st.button('Определить тональность')
 if result:
-    response = requests.post('http://localhost:8000/predict', 
+    response = requests.post('http://127.0.0.1:8000/predict', 
                              json={'text':text})
     st.write('**Результаты распознавания:**')
     st.write(response.json()['label'])
