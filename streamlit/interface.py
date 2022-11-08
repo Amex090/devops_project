0e7fc4cf791c004d331b365274b7483a48e26c7e
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-11-08 21:02:59 +0300

    Update interface.py

--- streamlit/interface.py
+++ streamlit/interface.py
@@ -6,6 +6,7 @@ import requests
 st.title('Определение тональности текста')
 text = st.text_input('Введите текст', 'Я люблю распределенные вычисления')
 result = st.button('Определить тональность')
+
 if result:
     response = requests.post('http://127.0.0.1:8000/predict', 
                              json={'text':text})
