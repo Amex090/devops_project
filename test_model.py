a21471fa99a4aaa9d07ee0b870cb1a79472eac4b
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-11-08 17:49:06 +0300

    Create test_model.py
    Add PyTests

---
+++ test_model.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_read_predict_positive():
+    response = client.post("/predict/", json={"text": "I like machine learning!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data["label"] == "sad"
+
+
+def test_read_predict_negative():
+    response = client.post("/predict/", json={"text": "I hate machine learning!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data["label"] == "angry"
